bd667ca6a2424ba8c3e47bc5bac28c5dafb0a675
gutsdk <e.zhuykov.02@bk.ru>
2025-01-20 15:48:42 +0400

    Первый коммит

---
+++ Solution/Program.cs
@@ -0,0 +1,47 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+class Program
+{
+    private static readonly ReceiverOptions receiverOptions = new ReceiverOptions
+    {
+        AllowedUpdates = { }
+    };
+
+    static async Task Main(string[] args)
+    {
+        string? botToken = Environment.GetEnvironmentVariable("TG_TOKEN");
+
+        if (string.IsNullOrEmpty(botToken))
+        {
+            Console.WriteLine("Ошибка: Токен не задан");
+            return;
+        }
+
+        var botClient = new TelegramBotClient(botToken);
+        var me = await botClient.GetMe();
+        Console.WriteLine($"Bot {me.FirstName} is running...");
+
+        botClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, CancellationToken.None);
+        Console.ReadLine();
+    }
+
+    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message && update.Message?.Text != null)
+        {
+            var message = update.Message;
+            Console.WriteLine($"Received a message from {message.Chat.Username}: {message.Text}");
+
+            await botClient.SendMessage(message.Chat.Id, $"Ты написал: {message.Text}");
+        }
+    }
+
+    private static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"Error occured: {exception.Message}");
+        return Task.CompletedTask;
+    }
+
+}
